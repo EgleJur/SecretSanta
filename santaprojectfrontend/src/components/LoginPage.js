af38de197a118217d753e345da0f1e11ac091bd1
Sakivan <benas.navikas@ktu.edu>
2023-12-08 18:46:27 +0200

    changes

--- santaprojectfrontend/src/components/LoginPage.js
+++ santaprojectfrontend/src/components/LoginPage.js
@@ -1,104 +1,117 @@
 import React, { useState, useContext } from "react";
-import { useHref, useNavigate } from "react-router-dom";
-// import AuthContext from '../AuthContext';
-import {
-  Button,
-  Form,
-  Grid,
-  Icon,
-  Input,
-  Segment,
-  Select,
-} from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../AuthContext";
+import { Button, Form, Grid, Segment } from "semantic-ui-react";
+import { jwtDecode } from "jwt-decode";
+
+export function getUsername() {
+  const token = localStorage.getItem("token");
+  if (token !== null) {
+    const decoded = jwtDecode(token);
+    console.log("decoded", decoded)
+    return decoded.sub;
+  }
+  return null;
+}
+
+export function setUsernameIndex() {
+  const username = getUsername();
+  const hash = username
+    .split("")
+    .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+
+  const index = (hash % 25) + 1;
+  localStorage.setItem("avatar", index);
+  return index;
+}
 
 export function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [error, setError] = useState("");
   const [user, setUser] = useState("");
   const [role, setRole] = useState("");
   const navigate = useNavigate();
-
+  const { appState, setAppState } = useContext(AuthContext);
   const [state, setState] = useState({
+    email: "",
     username: "",
     password: "",
     loading: false,
   });
 
-  const listUrl = useHref("/menus/list");
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  //   const handleSubmit = (event) => {
-  //     if(!appState.isAuthenticated){
-  //     setAppState({ type: "LOADING", value: true })
-  //     event.preventDefault();
+  const handleRegisterClick = () => {
+    navigate(`/auth/register`);
+  };
 
-  //     const credentials = {
-  //       username: username,
-  //       password: password
-  //     };
+  const handleSubmit = (event) => {
+    if (!appState.isAuthenticated) {
+      setAppState({ type: "LOADING", value: true });
+      event.preventDefault();
 
-  // fetch('api/v1/login', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     // 'Accept': 'application/json'
-  //   },
-  //   body: JSON.stringify(credentials)
-  // })
-  //   .then(applyResult)
-  //   .catch((error) => {
-  //     setError('An error occurred. Please try again later.');
-  //   })
-  // }
-  // else{
-  //     alert("You are already logged in");
-  // setAppState({ type: "AUTHENTICATED", value: true })
-  //     }
-  //   };
+      const credentials = {
+        email: email,
+        password: password,
+      };
 
-  //   const applyResult = (result) => {
-  //     const clear = () => {
-  //         clearForm();
-  //     }
-  //         if (result.ok) {
-  //            setAppState({type: "LOGIN", value: true })
-  //            navigate('/menus/list');
-  //         } else {
-  //           setError('Login failed. Please try again.');
-  //         }
-  //   }
-  //   const clearForm = () => {
-  //     setState({
-  //         ...state, user: '',
-  //         password: null,
-  //     })
-  // }
+      fetch("/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+        body: JSON.stringify(credentials),
+      })
+        .then(applyResult)
+        .catch((error) => {
+          setError("An error occurred. Please try again later.");
+        });
+    } else {
+      alert("You are already logged in");
+      setAppState({ type: "AUTHENTICATED", value: true });
+    }
+  };
 
-  // const { appState, setAppState } = useContext(AuthContext)
+  const applyResult = (result) => {
+    if (result.ok) {
+      result.json().then((data) => {
+        console.log("DATA:", data);
+        localStorage.setItem("token", data);
+        setUsernameIndex();
+        console.log("USERNAME INDEX:", getUsername());
+        console.log("LOCAL STORAGE", localStorage);
+        setAppState({ type: "LOGIN", value: true });
+        navigate(`/users/${data.userId}`);
+      });
+    } else {
+      setError("Login failed. Please try again.");
+    }
+  };
 
   return (
     <Grid centered columns={2}>
       <Grid.Column>
         <h2>Log in</h2>
-        <Segment color="red" inverted>
-          <Form color="red" inverted>
+        <Segment style={{ backgroundColor: "rgb(250, 110, 110" }} inverted>
+          <Form style={{ backgroundColor: "rgb(250, 110, 110" }} inverted>
             <Form.Group widths="equal">
               <Form.Field>
-                <label>User name</label>
+                <label>Email address</label>
                 <input
-                  type="text"
-                  name="username"
-                  placeholder="User name"
-                  value={username}
-                  onChange={handleUsernameChange}
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={handleEmailChange}
                 />
               </Form.Field>
               <Form.Field>
@@ -113,12 +126,22 @@ export function LoginPage() {
               </Form.Field>
             </Form.Group>
             <Button
+              fluid
               type="submit"
               color="white"
               inverted
-              //   onClick={handleSubmit}
+              onClick={handleSubmit}
             >
-              Confirm
+              Login
+            </Button>
+            <Button
+              className="create mt-2"
+              color="white"
+              inverted
+              fluid
+              onClick={handleRegisterClick}
+            >
+              Register
             </Button>
           </Form>
         </Segment>
--- src/main/java/Secret/Santa/Secret/Santa/SecretSantaApplication.java
+++ src/main/java/Secret/Santa/Secret/Santa/SecretSantaApplication.java
@@ -1,13 +1,18 @@
 package Secret.Santa.Secret.Santa;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Secret Santa API", version = "1.0",
         description = "Secret Santa API"))
+@SecurityScheme(name = "arapi", scheme = "bearer", bearerFormat = "JWT", type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER, description = "JWT Authorization header using Bearer scheme")
 public class SecretSantaApplication {
 
     public static void main(String[] args) {
